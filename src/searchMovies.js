0190887b0a737011a7bb971a96f6a58888bfd0e8
Malek Zarzour <malekz@maleks-mbp.localdomain>
2020-09-06 00:58:36 +0200

    created api request to tmdb

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -4,7 +4,18 @@ const SearchMovies = () => {
   const searchMovies = async (e) => {
     e.preventDefault();
     console.log('submitting');
+
+    const query = 'Jurassic Park';
+    const url = `https://api.themoviedb.org/3/movi/550?api_key=dfcbd1f7d88db283a58a96ee2cd1f292&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  } catch(err) {
+    console.log(err)
   };
+  
   return (
     <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
